efb4365680d14499bd97ce4e77e804cdeef36001
OmarLaham <p.omarlahham@gmail.com>
2025-05-14 21:45:41 +0200

    comment out run.sh content

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -e
+# set -e
 
-# Add local pip bin path to PATH (for pip user installs)
-export PATH="$HOME/.local/bin:$PATH"
+# # Add local pip bin path to PATH (for pip user installs)
+# export PATH="$HOME/.local/bin:$PATH"
 
-echo "Installing requirements..."
-pip install --user -r requirements.txt
-pip freeze > requirements.txt
+# echo "Installing requirements..."
+# pip install --user -r requirements.txt
+# pip freeze > requirements.txt
 
-echo "Running Streamlit..."
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
-
-
-# pip install -r requirements.txt 
+# echo "Running Streamlit..."
 # streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+
+
+# # pip install -r requirements.txt 
+# # streamlit run app.py --server.port 8000 --server.address 0.0.0.0
